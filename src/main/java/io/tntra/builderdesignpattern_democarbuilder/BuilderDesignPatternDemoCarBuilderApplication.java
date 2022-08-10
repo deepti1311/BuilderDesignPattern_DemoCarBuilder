dbe2af5a82dc42bede1ed31e1ecb59bdb370272d
deepti1311 <deeptijakhotra@gmail.com>
2022-08-10 13:01:17 +0530

    "initial files"

---
+++ src/main/java/io/tntra/builderdesignpattern_democarbuilder/BuilderDesignPatternDemoCarBuilderApplication.java
@@ -0,0 +1,13 @@
+package io.tntra.builderdesignpattern_democarbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderDesignPatternDemoCarBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderDesignPatternDemoCarBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tntra/builderdesignpattern_democarbuilder/BuilderDesignPatternDemoCarBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tntra.builderdesignpattern_democarbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderDesignPatternDemoCarBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
